e34566c7e1b09e35c28ca7ed1ebaaf622a53030d
Fernando Carranza <foreverfas_fc@hotmail.com>
2024-11-29 21:25:14 -0600

    :tada: first commit

---
+++ database/migrations/2024_11_28_073700_create-products-table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('name');
+            $table->mediumText('description');
+            $table->decimal('price', 10, 2);
+            $table->string('img_url');
+            $table->timestamps();
+            $table->softDeletes('deleted_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
---
+++ database/migrations/2024_11_29_010507_create_inventories_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('inventories', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->foreignUuid('product_id')->references('id')->on('products');
+            $table->integer('quantity');
+            $table->enum('movement', ['entrada', 'salida']);
+            $table->mediumText('description')->nullable();
+            $table->timestamps();
+            $table->softDeletes('deleted_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('inventories');
+    }
+};
